c200e559152efcc11378189e91f6e5bd8e832b83
m-berdnikov-regium <maxim.berdnikov@regium.com>
2021-04-17 17:44:44 +0300

    fix

--- src/js/prediction.js
+++ src/js/prediction.js
@@ -1,3 +1,5 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', function () {
 
     let cookieCardArray = Cookies.get('tenTarotCards');
--- src/js/validation.js
+++ src/js/validation.js
@@ -1,3 +1,5 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', () => {
 
     let formWrapper = document.querySelector('.js-form-wrapper');
